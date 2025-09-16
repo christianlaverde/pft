d15670a2211e7ca16c9d5a5aa7334cabf5a84344
Christian LaVerde <chrislaverde1@gmail.com>
2025-09-16 10:05:35 -0500

    Update test_models to setup the test db

--- test_models.py
+++ test_models.py
@@ -12,13 +12,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 
-def test_models():
+def setup_test_db(app):
     with app.app_context():
-        print('Creating DB tables...\n')
         db.drop_all()
         db.create_all()
 
-        print("Creating Accounts...")
         checking = Account(account_name="Checking",
                            account_type=AccountType.ASSET)
         salary = Account(account_name="Salary",
@@ -30,22 +28,9 @@ def test_models():
         equity = Account(account_name="Equity",
                          account_type=AccountType.EQUITY)
         accounts = [checking, salary, groceries, credit, equity]
-
         db.session.add_all(accounts)
         db.session.commit()
 
-        print(f'Created: {checking}')
-        print(f'\tNormalBalance: {checking.normal_balance}')
-        print(f'Created: {salary}')
-        print(f'\tNormalBalance: {salary.normal_balance}')
-        print(f'Created: {groceries}')
-        print(f'\tNormalBalance: {groceries.normal_balance}')
-        print(f'Created: {credit}')
-        print(f'\tNormalBalance: {credit.normal_balance}')
-        print(f'Created: {equity}')
-        print(f'\tNormalBalance: {equity.normal_balance}')
-
-        print('\nCreating Transactions...')
         opening_balance_transaction = Transaction(description='Opening Balance',
                                                   date=datetime.now(),
                                                   amount=Decimal('1750.50'),
@@ -69,17 +54,3 @@ def test_models():
 
         db.session.add_all([opening_balance_transaction, groceries_transaction, income_transaction, credit_transaction])
         db.session.commit()
-
-        print(f'Created: {opening_balance_transaction}')
-        print(f'Created: {groceries_transaction}')
-        print(f'Created: {income_transaction}')
-        print(f'Created: {credit_transaction}')
-
-        print('\nCalculating Account Balances...')
-        for account in accounts:
-            balance = account.get_balance()
-            nb = 'DR' if account.normal_balance == NormalBalance.DEBIT else 'CR'
-            print(f'{account} Balance: ${balance}{nb}')
-
-
-test_models()
