87a0f8c81ed1229ade7cb7f17cc41a20c83bdcbb
Christian LaVerde <chrislaverde1@gmail.com>
2025-11-25 15:14:59 -0600

    Remove debug print statements

--- helper_funcs.py
+++ helper_funcs.py
@@ -7,23 +7,17 @@ import os
 
 def setup_test_db(app):
     with app.app_context():
-
-        print(f'DEBUG: SETUP DB URI: {app.config['SQLALCHEMY_DATABASE_URI']}')
-        
         db.drop_all()
         db.create_all()
 
         sql_seed_data_file_path = os.path.join(os.path.dirname(__file__), 'seed_data.sql')
-
         with open(sql_seed_data_file_path, 'r') as f:
             sql_script = f.read()
 
         statements = [s.strip() for s in sql_script.split(';') if s.strip()]
         for statement in statements:
             db.session.execute(text(statement))
+
         db.session.commit()
 
-        from models import Account, Transaction
-        print(f'AFTER SETUP: {Account.query.count()}')
-        print(f'AFTER SETUP: {Transaction.query.count()}')
 
