7e58ee00c02e299f79fae47c41b304f3e4afd547
Christian LaVerde <chrislaverde1@gmail.com>
2025-11-26 15:45:50 -0600

    Add dev db uri to .env and logic in __init__.py to check

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,12 @@ def create_app():
                 static_folder='static',
                 template_folder='templates')
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'SECRET-DEV-KEY'
     app.config['APP_ENV'] = os.environ.get('APP_ENV')
+    if app.config['APP_ENV'] == 'DEV':
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DEV_DATABASE_URI')
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'SECRET-DEV-KEY'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
